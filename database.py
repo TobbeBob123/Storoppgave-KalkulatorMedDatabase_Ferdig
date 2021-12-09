e00f1355bc5a82ca957d28092c89905471874893
Tobias <tobiasfi@viken.no>
2021-12-09 14:06:43 +0100

    Ferdig

--- database.py
+++ database.py
@@ -5,7 +5,8 @@ from mysql.connector import connect
 from mysql.connector import Error
 
 
-
+#dette er får la andre å få tilgang til databasen med sin egen mysql connection, 
+# samt at dette gjør det mye mer sikkert.
 try:
     db=mysql.connector.connect(
         host="localhost",
